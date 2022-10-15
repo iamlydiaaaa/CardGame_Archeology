2a0be34918b7bac8455a5737551542093a1aecf6
iamlydiaaaa <kg0930@naver.com>
2022-10-16 01:52:40 +0900

    version3

--- Assignment2_Archeology/Assignment2_Archeology/Form1.cs
+++ Assignment2_Archeology/Assignment2_Archeology/Form1.cs
@@ -9,47 +9,91 @@ namespace Assignment2_Archeology
     public partial class Form1 : Form
     {
         //The number of cards which players and trade center belongs to
-        public static int PLAYER_SIZE = 5;
+        public static int PLAYER_SIZE = 4;
         public static int TRADE_SIZE = 5;
+        public static int NUM_CARDS = 67;
+
         protected System.Resources.ResourceManager resource_manager;
 
-        int cardIndex = 0;
-        System.Timers.Timer t;
+        public static int cardIndex = 0;
+        //System.Timers.Timer t;
 
-        List<Card> dealt_hand = new List<Card>();
+        List<Card> deck_card = new List<Card>();
+        List<Card> marketPlace = new List<Card>();
+        List<Card> Listplayer1 = new List<Card>();
+        List<Card> Listplayer2 = new List<Card>();
         Deck deck;
+
+        bool trade = false;
 
         public Form1()
         {
             InitializeComponent();
             resource_manager = Properties.Resources.ResourceManager;
             deck = new Deck();
-            GetAHand();
         }
 
         private void GetAHand()
         {
-            dealt_hand.Clear();
-            //deck.ShuffleCards();
-            for(int i = 0; i < PLAYER_SIZE; i++)
+
+            deck_card.Clear();
+            marketPlace.Clear();
+            Listplayer1.Clear();
+            Listplayer2.Clear();
+
+
+            //make new random card lists
+            for (int i = 0; i < NUM_CARDS; i++)
             {
                 Card card = deck.GetCard(i);
                 //card.UnFlip();
-                dealt_hand.Add(card);
-                Console.WriteLine("GetAHand: " + card);
+                deck_card.Add(card);
+                Console.WriteLine("deck_card[" + i + "] = " + deck_card[i]);
+                cardIndex++;
             }
+            deck.ShuffleCards();
+        }
+        private void getRandomCards(Graphics graphics, List<Card> playerList, int CardSize, int cp)
+        {
+            Random rand = new Random();
 
+            int CardPos = cp;
+            int CARD_GAP = 80;
+
+            //draw cards
+            for (int i = 0; i < CardSize; i++)
+            {
+                int randNum = rand.Next(1, cardIndex);
+                playerList.Add(deck_card[randNum]);
+                Card c = playerList[i];
+
+                deck_card.RemoveAt(randNum);
+                cardIndex--;
+                c.DrawCard(graphics, pictureBoxPlayer1, CardPos);
+                CardPos = CardPos + CARD_GAP;
+                labelLeftover.Text = cardIndex.ToString();
+            }
+            Console.WriteLine(cardIndex.ToString());
         }
         private void buttonStart_Click(object sender, EventArgs e)
         {
-            t = new System.Timers.Timer();
-            t.Elapsed += OnTimedEvent;
-            t.Interval = 10;
-            t.Enabled = true;
-            t.Start();
+            Graphics p1 = pictureBoxPlayer1.CreateGraphics();
+            Graphics p2 = pictureBoxPlayer2.CreateGraphics();
+            Graphics p3 = pictureBoxCenter.CreateGraphics();
+            //t = new System.Timers.Timer();
+            //t.Elapsed += OnTimedEvent;
+            //t.Interval = 10;
+            //t.Enabled = true;
+            //t.Start();
 
             cardIndex = 0;
             GetAHand();
+            getRandomCards(p1, Listplayer1,PLAYER_SIZE,10);
+            getRandomCards(p2, Listplayer2, PLAYER_SIZE,10);
+            getRandomCards(p3, marketPlace,5,10);
+
+            Console.WriteLine(marketPlace.ToString());
+
         }
 
         // Specify what you want to happen when the Elapsed event is raised.
@@ -63,25 +107,20 @@ namespace Assignment2_Archeology
 
         private void pictureBoxPlayer1_Paint(object sender, PaintEventArgs e)
         {
-            //deck.ShuffleCards();
-            for (int i = 1; i < PLAYER_SIZE; i++)
-            {
-                Card card = deck.GetCard(i);
-                //card.UnFlip();
-                dealt_hand.Add(card);
-                Console.WriteLine("GetAHand: " + dealt_hand[i]);
-            }
+        }
 
-            Graphics g = e.Graphics;
-            for (int i = 1; i < PLAYER_SIZE; i++)
-            {
-                Card c = dealt_hand[i];
-                Console.WriteLine("dealt_hand[i] = " + dealt_hand[i]);
-                c.DrawCard(g, pictureBoxPlayer1, i);
-            }
+        private void buttonDrawACard_Click(object sender, EventArgs e)
+        {
+            Graphics p1 = pictureBoxPlayer1.CreateGraphics();
+            getRandomCards(p1, Listplayer1, 1,350);
+        }
 
-            if (cardIndex == PLAYER_SIZE) t.Stop();
-            cardIndex++;
+        private void buttonTrade_Click(object sender, EventArgs e)
+        {
+            //trade활성화
+            trade = true;
+
+
         }
     }
 }
